4ae3fe8a242738c8407ddebbaaf7f977056753d2
Achintya72 <achintya.agrawal.90@gmail.com>
2024-10-27 02:56:46 -0700

    feat: added new methods for user editing

--- src/backend/studentContext.tsx
+++ src/backend/studentContext.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { createContext, PropsWithChildren, useCallback, useContext, useEffect, useState } from "react";
+import { createContext, Dispatch, PropsWithChildren, SetStateAction, useCallback, useContext, useEffect, useState } from "react";
 import { Student, StudentID, Team } from "./types";
 import LoginContext from "./loginContext";
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
-import { db } from "./firebase";
+import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
+import { auth, db } from "./firebase";
 
 interface StudentContextData {
     students: Student[],
     populated: boolean,
     myTeams: Team[],
     populatedTeams: boolean,
-    getMe: () => Student | undefined
+    getMe: () => Student | undefined,
+    addNewUser: (id: string, data: Student, changeError: Dispatch<SetStateAction<string | null>>) => void
+    editStudent: (data: Student, changeError: Dispatch<SetStateAction<string | null>>) => void
 }
 
 const StudentContext = createContext<StudentContextData>({
@@ -20,6 +22,8 @@ const StudentContext = createContext<StudentContextData>({
     populatedTeams: false,
     getMe: () => undefined,
     populated: false,
+    addNewUser: () => { },
+    editStudent: () => {}
 });
 
 function StudentContextProvider({ children }: PropsWithChildren) {
@@ -28,56 +32,102 @@ function StudentContextProvider({ children }: PropsWithChildren) {
     const [populatedTeams, changePopulatedTeams] = useState<boolean>(false);
     const [myTeams, changeMyTeams] = useState<Team[]>([]);
     const { authUser } = useContext(LoginContext);
-    
+
     const getMe = useCallback(() => {
         return students.find(s => s.id === authUser?.uid);
     }, [students, authUser]);
-     
+
+    const addNewUser = async (id: string, data: Student, changeError: Dispatch<SetStateAction<string | null>>) => {
+        console.log(id);
+        try {
+            await setDoc(doc(db, "students", id), {
+                ...data
+            });
+            changeStudents(prev => [...prev, { ...data, id: id }]);
+        } catch (err) {
+            console.log("Uh oh", err);
+            changeError("Something went wrong. Try again later");
+        }
+    }
+
+    const editStudent = async (data: Student, changeError: Dispatch<SetStateAction<string | null>>) => {
+        try {
+            await setDoc(doc(db, "students", data.id), {
+                ...data
+            }, { merge: true});
+            changeStudents(prev => {
+                const newPrev = prev.filter(s => s.id !== data.id);
+                return [...newPrev, data];
+            })
+        } catch(err) {
+            console.log(err);
+            changeError("Something's wrong. Try again later");
+        }
+    }
+
     useEffect(() => {
         changePopulated(false);
-        const getStudents = async () => {
+        const getStudents = async (uid: string) => {
             const response = await getDocs(collection(db, "students"));
             const newStudents = response.docs.map(doc => {
-                return {...doc.data(), id: doc.id as StudentID} as Student
+                return { ...doc.data(), id: doc.id as StudentID } as Student
             });
-            changeStudents(newStudents);
+            if (newStudents.find(s => s.id === uid) == undefined) {
+                await setDoc(doc(db, "students", uid), {
+                    id: uid,
+                    name: "",
+                    grade: 9,
+                    onboarded: false,
+                    teams: []
+                });
+                changeStudents([...newStudents, {
+                    id: uid,
+                    name: "",
+                    grade: 9,
+                    onboarded: false,
+                    teams: []
+                }]);
+            } else {
+                changeStudents(newStudents);
+
+            }
             changePopulated(true);
         }
-        
-        if(authUser) {
-            getStudents();
+
+        if (authUser) {
+            getStudents(authUser.uid);
         } else {
             changeStudents([]);
             changePopulated(true);
         }
     }, [authUser, changeStudents, changePopulated]);
-    
+
 
     useEffect(() => {
         const getMyNewTeams = async () => {
             const myProfile = getMe();
-            if(myProfile != undefined) {
+            if (myProfile != undefined) {
                 const promises: Promise<Team | null>[] = myProfile.teams.map(async ({ teamId }) => {
                     const teamRef = doc(db, "teams", teamId);
                     const docSnap = await getDoc(teamRef);
 
-                    return docSnap.exists() ? ({...docSnap.data(), id: docSnap.id} as Team) : null;
+                    return docSnap.exists() ? ({ ...docSnap.data(), id: docSnap.id } as Team) : null;
                 });
 
                 const data: Team[] = (await Promise.all(promises)).filter(team => team != null);
                 changeMyTeams(data);
                 changePopulatedTeams(true);
-                
+
             }
         }
 
-        if(authUser) {
+        if (authUser) {
             getMyNewTeams();
         } else {
             changeMyTeams([]);
             changePopulatedTeams(true);
         }
-    }, [authUser, students, getMe, changeMyTeams, changePopulatedTeams ]);
+    }, [authUser, students, getMe, changeMyTeams, changePopulatedTeams]);
 
 
     const values: StudentContextData = {
@@ -85,7 +135,9 @@ function StudentContextProvider({ children }: PropsWithChildren) {
         populated,
         myTeams,
         populatedTeams,
-        getMe
+        getMe,
+        addNewUser,
+        editStudent
     }
 
     return (
