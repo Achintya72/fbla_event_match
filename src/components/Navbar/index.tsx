b14cb8211362a0eebb1c1d77691f70c9bad8c6fc
Achintya72 <achintya.agrawal.90@gmail.com>
2024-10-26 17:21:44 -0700

    feat: added link to login page in Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,11 +6,13 @@ import Button from "../Button";
 import { useState } from "react";
 import { List } from "@phosphor-icons/react";
 import { X } from "@phosphor-icons/react/dist/ssr";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
     const [showMenu, toggleMenu] = useState<boolean>(false);
+    const router = useRouter();
     return (
-        <nav className="flex relative items-center justify-between w-full text-white">
+        <nav className="flex relative z-40 items-center justify-between w-full text-white">
             <div className="flex gap-[20px]">
                 <Image src="/fbla.png" width={30} height={30} alt="FBLA Logo" />
                 <h4 className="font-space text-2xl font-bold">FBLA Event Match</h4>
@@ -19,18 +21,20 @@ export default function Navbar() {
                 <Link href="/" className="font-raleway font-medium" >Home</Link>
                 <Link href="/dashboard" className="font-raleway font-medium" >Dashboard</Link>
             </div>
-            <Button className="hidden md:block">Log In</Button>
-            {!showMenu ?
-                <List size={24} onClick={() => toggleMenu(true)} className="flex md:hidden" /> :
-                <X size={24} className="flex md:hidden" onClick={() => toggleMenu(false)} />
-            }
-            {showMenu &&
-                <div className="absolute z-50 right-[0px] top-[45px] text-right border border-[rgba(255,255,255,0.3)] flex gap-[20px] bg-background p-[20px] rounded-[8px] flex-col md:hidden">
-                    <Link href="/" className="font-raleway font-medium" >Home</Link>
-                    <Link href="/dashboard" className="font-raleway font-medium" >Dashboard</Link>
-                    <Button>Log In</Button>
-                </div>
-            }
-        </nav>
+            <Button className="hidden md:block" onClick={() => router.push("/login")}> Log In</Button>
+            {
+        !showMenu ?
+        <List size={24} onClick={() => toggleMenu(true)} className="flex md:hidden" /> :
+        <X size={24} className="flex md:hidden" onClick={() => toggleMenu(false)} />
+    }
+    {
+        showMenu &&
+        <div className="absolute z-50 right-[0px] top-[45px] text-right border border-[rgba(255,255,255,0.3)] flex gap-[20px] bg-background p-[20px] rounded-[8px] flex-col md:hidden">
+            <Link href="/" className="font-raleway font-medium" >Home</Link>
+            <Link href="/dashboard" className="font-raleway font-medium" >Dashboard</Link>
+            <Button onClick={() => router.push("/login")} >Log In</Button>
+        </div>
+    }
+        </nav >
     )
 }
