d15fd35556e01dc0f98108d50e4ec104fa33d57a
Achintya72 <achintya.agrawal.90@gmail.com>
2024-10-26 17:21:17 -0700

    feat: added text-button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,24 +6,27 @@ import { ButtonHTMLAttributes, ReactNode } from "react";
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     loading?: boolean,
     disabled?: boolean,
-    variant?: "primary" | "secondary",
+    variant?: "primary" | "secondary" | "text",
     icon?: ReactNode
 }
 
 const variantColors = {
     "primary": "bg-blue",
-    "secondary": "bg-yellow"
+    "secondary": "bg-yellow",
+    "text": "bg-transparent"
 }
 
 const variantDropShadows = {
     primary: "hover:drop-shadow-[0_4px_16px_rgba(100,144,231,0.25)]",
-    secondary: "hover:drop-shadow-[0_4px_16px_rgba(197,159,86,0.25)]"
-}
+    secondary: "hover:drop-shadow-[0_4px_16px_rgba(197,159,86,0.25)]",
+    text: "hover:bg-[rgba(255,255,255,0.05)]"
+}        
 
-export default function Button({ className, icon = null, loading = false, disabled = false, variant = "primary", children, ...props }: ButtonProps) {
+export default function Button({ className, onClick, icon = null, loading = false, disabled = false, variant = "primary", children, ...props }: ButtonProps) {
     return (
         <button 
             {...props}
+            onClick={onClick}
             className={
             classNames(
                 "px-[16px] py-[10px] rounded-[8px] font-raleway font-bold",
